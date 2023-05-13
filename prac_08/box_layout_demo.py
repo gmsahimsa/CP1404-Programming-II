ac781f3fd46c999a5a0d9261146c51254d15ae2d
AhimsaGMS <ytahimsavidur@gmail.com>
2023-05-13 22:26:05 +0800

    Box Layout Kivy

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        self.root.ids.output_label.text = "Hello "
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
+
+
+BoxLayoutDemo().run()
