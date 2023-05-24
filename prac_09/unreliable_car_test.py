c5232838ca442f229696cc6609036523b177700e
GMS Ahimsa [ Blade ] <ytahimsavidur@gmail.com>
2023-05-24 08:57:09 +0800

    Week 10 - Feedback

--- prac_09/unreliable_car_test.py
+++
@@ -1,8 +0,0 @@
-from unreliable_car import UnreliableCar
-
-my_car = UnreliableCar("Unreliable Car", 100, 50)  # reliability set to 50%
-distance_driven = my_car.drive(50)
-print("Distance driven:", distance_driven)
-
-distance_driven = my_car.drive(50)
-print("Distance driven:", distance_driven)
